22390ad3915f5b41f0be998756669bae8da6684f
benbraikilyas <benbraikilyas9@gmail.com>
2026-08-19 16:16:50 +0100

    edit file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Allow your phone/other devices on the local network to access Next.js dev resources
+  allowedDevOrigins: ["192.168.100.25"],
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
